c11caf28b8425811dc6962d5f76bd66b45e9975e
Arsen Shikhabov <arsen.sh@MacBook-Air-Arsen.local>
2024-10-09 19:21:41 +0300

    feat: add solution for getPositivesCount

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -305,8 +305,10 @@ function get3TopItems(arr) {
  *   [ null, 1, 'elephant' ] => 1
  *   [ 1, '2' ] => 1
  */
-function getPositivesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getPositivesCount(arr) {
+  return arr
+    .filter((item) => typeof item === 'number')
+    .filter((item) => item > 0).length;
 }
 
 /**
